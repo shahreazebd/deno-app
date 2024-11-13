de2f00745360d8f061ad881a36a275c7ca2d16f2
shahreazebd <shahreaz@evidentbd.com>
2024-11-13 15:14:40 +0600

    remove

--- server.ts
+++ server.ts
@@ -6,4 +6,8 @@ app.get("/", (_, res) => {
   res.json({ msg: "hello from deno" });
 });
 
+app.get("/health", (_, res) => {
+  res.json({ msg: "OK", timestamp: Date.now() });
+});
+
 app.listen(8081);
